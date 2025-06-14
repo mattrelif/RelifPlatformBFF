ce24eac9ea4bc7439822e5a56d6025d39895c321
Matt Souza <matt@relifaid.org>
2025-06-14 18:00:26 -0300

    Update organization.go

--- models/organization.go
+++ models/organization.go
@@ -11,6 +11,8 @@ type Organization struct {
 	ID               string    `bson:"_id,omitempty"`
 	Name             string    `bson:"name,omitempty"`
 	Description      string    `bson:"description,omitempty"`
+	Logo             string    `bson:"logo,omitempty"`
+	AreasOfWork      []string  `bson:"areas_of_work,omitempty"`
 	Address          Address   `bson:"address,omitempty"`
 	Type             string    `bson:"type,omitempty"`
 	Status           string    `bson:"status,omitempty"`
@@ -25,6 +27,8 @@ func (organization *Organization) ToEntity() entities.Organization {
 		ID:               organization.ID,
 		Name:             organization.Name,
 		Description:      organization.Description,
+		Logo:             organization.Logo,
+		AreasOfWork:      organization.AreasOfWork,
 		Address:          organization.Address.ToEntity(),
 		Type:             organization.Type,
 		Status:           organization.Status,
@@ -40,6 +44,8 @@ func NewOrganization(entity entities.Organization) Organization {
 		ID:          primitive.NewObjectID().Hex(),
 		Name:        entity.Name,
 		Description: entity.Description,
+		Logo:        entity.Logo,
+		AreasOfWork: entity.AreasOfWork,
 		Address:     NewAddress(entity.Address),
 		Type:        utils.ManagerOrganizationType,
 		Status:      utils.ActiveStatus,
@@ -52,6 +58,8 @@ func NewUpdatedOrganization(entity entities.Organization) Organization {
 	return Organization{
 		Name:             entity.Name,
 		Description:      entity.Description,
+		Logo:             entity.Logo,
+		AreasOfWork:      entity.AreasOfWork,
 		Address:          NewAddress(entity.Address),
 		Type:             entity.Type,
 		OwnerID:          entity.OwnerID,
